d91fb640a41657641282f9daae8f471c1ed2b970
ron <business.mingyukim@gmail.com>
2019-04-10 18:24:15 +0900

    rest api

---
+++ src/common/Constants.js
@@ -0,0 +1,9 @@
+export const IS_DEV_MODE = process.env.NODE_ENV === 'development';
+
+export const SERVER_URL = 'http://14.32.136.156:7410';
+
+export const PROJECT_NAME = 'insta';
+
+export const USER_DATA_STORAGE_NAME = `${PROJECT_NAME}_cli`;
+
+export const TEMP_NAME = 'userData';
